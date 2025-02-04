31efe135e36ae2005b1c3f7f86f02590da5824e1
Carlos Andres Zabala Baquero <cazabalab@unal.edu.co>
2025-02-04 14:39:31 -0500

    Inicializando repositorio con compotentes nav y top header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopHeaderComponent } from "./components/commons/top-header/top-header.component";
+import { NavbarComponent } from './components/commons/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopHeaderComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

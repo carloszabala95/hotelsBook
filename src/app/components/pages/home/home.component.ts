ed3c269082f6e8ced5633d1f1883d10c6c876f26
Carlos Andres Zabala Baquero <cazabalab@unal.edu.co>
2025-02-09 21:01:01 -0500

    agregar componente navbar junto con construccion del home y listing

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { DpDatePickerModule } from 'ng2-date-picker';
+import { HotelListingComponent } from '../hotel-listing/hotel-listing.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    DpDatePickerModule,
+    HotelListingComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  datePickerConfig = {
+    format : 'YYYY-MM-DD'
+  };
 }
